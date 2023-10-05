860037d25a4f4f083b620dc8bf4f67b61ae9dc08
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-10-05 19:17:09 -0400

    Complete exercise 3 but kinda stupidly

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let letters = text.split("");
+    let reverse = [];
+    for (let i = 0; i < text.length; i++) {
+        reverse[i] = letters[(text.length - 1 - i)];
+    }
+    return reverse.join("");
 };
 
 // Do not edit below this line
