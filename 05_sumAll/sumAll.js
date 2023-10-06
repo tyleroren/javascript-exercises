a95bc512f7e621f173fcbbb8104d85cd363b1428
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-10-06 16:24:28 -0400

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(...num) {
+    for (const n of num) {
+        if (typeof n != "number") {
+            return "ERROR";
+        } else if (n < 0) {
+            return "ERROR";
+        }
+    }
+    total = 0
+    num.sort((a, b) => a-b);
+    for (let i = num[0]; i <= num[1]; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
