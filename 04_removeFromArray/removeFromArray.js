10ddfcf1c32556634cbcc84678af57df2a5f206b
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-10-05 20:45:04 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    return array.filter((toDel) => !remove.includes(toDel));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
