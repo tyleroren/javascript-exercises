700108c54613d990d910e56a9ba87874667f0893
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-10-07 07:19:50 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const c = (f - 32) * (5/9);
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const f = (c * (9/5) + 32);
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
