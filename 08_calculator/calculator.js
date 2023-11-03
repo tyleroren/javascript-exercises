25769c2058ae345e9d2606cdad8bbeccda035a1f
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-02 21:37:47 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(...arr) {
+	return arr.reduce((sum, current) => sum - current, (arr[0] * 2));
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  if (a === 0) return 1;
+  for(let i = 1; a >= i; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
