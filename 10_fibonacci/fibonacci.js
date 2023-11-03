b020aa1d168b08f36bb2284d65696248b0caf77c
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-03 17:51:21 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(seq) {
+    let current = 1;
+    let last = 0;
+    let next = 1;
+    if (seq < 0) return "OOPS";
+    for (let i = 0; i !== Number(seq); i++) {
+        // console.log(`last: ${last}, current: ${current}, next: ${next}`)
+        next = last + current;
+        last = current;
+        current = next;
+    }
+    return last;
 };
 
 // Do not edit below this line
