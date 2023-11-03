a8a48625b8543c021759ce5f24ff08c420e6e4dd
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-03 07:01:18 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.toLowerCase().replaceAll(/\W/gi, "").split('');
+    return arr.join('') === arr.reverse().join('');
 };
 
 // Do not edit below this line
