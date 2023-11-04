7edf44c29bf5f1d8840ed5f0dfe43eb4e9591a69
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-04 10:05:37 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.sort((a, b) => {
+        const currentYear = (new Date()).getFullYear();
+        let last = 0;
+        let next = 0;
+        if ("yearOfDeath" in a) {
+            last = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            last = currentYear - a.yearOfBirth;
+        }
+        if ("yearOfDeath" in b) {
+            next = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            next = currentYear - b.yearOfBirth;
+        }
+        return last > next ? -1 : 1;
+    });
+    return oldest[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
